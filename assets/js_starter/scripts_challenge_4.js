1c60ee83c0168c12915491dd841cc8c4d4696400
Ash Luchowa <ashwinluchowa@gmail.com>
2025-09-18 13:24:57 +0800

    Find if number 5 is in array + if true, print out up to number 5, else display message

--- assets/js_starter/scripts_challenge_4.js
+++ assets/js_starter/scripts_challenge_4.js
@@ -4,11 +4,17 @@ let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
 let resultIndex = numbers.findIndex((x) => x === 5);
-console.log(resultIndex); // 4
+// console.log(resultIndex); // 4
 
 // If the number 5 is found in the array
-
+if(resultIndex !== -1) {
   // Console log each number in the array up to and including 5
-
-  // Output a message if the number 5 is not in the array
-  console.log("Number not in array")
+  numbers.forEach((x,y) => {
+    if(y <= resultIndex) {
+      console.log(x);
+    }
+  });
+} else {
+    // Output a message if the number 5 is not in the array
+    console.log("Number not in array")
+}
