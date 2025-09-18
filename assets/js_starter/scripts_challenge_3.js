f32f4c98a191cac36a562d39537008a7807ac1cf
Ash Luchowa <ashwinluchowa@gmail.com>
2025-09-18 12:52:03 +0800

    Check if 5 is included + filter out numbers less than 5

--- assets/js_starter/scripts_challenge_3.js
+++ assets/js_starter/scripts_challenge_3.js
@@ -3,12 +3,17 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Check if the array includes the number 5
+let checkNumber5 = numbers.includes(5); //true
 
 // If true, filter out numbers less than 5
+if(checkNumber5) {
+    let resultArray = numbers.filter((x) => x >= 5);
+    console.log(resultArray); // 5,6
+}
 
 // Then square each number in the resulting array
 
 // Output the final array
 
 // Output a message if the number 5 is not in the array
-console.log("The number 5 is not in the array.");
+// console.log("The number 5 is not in the array.");
